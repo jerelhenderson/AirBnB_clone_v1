81bb9f405fa48810eb5ee74fc67d1b89abaa38c0
Jacob Biehler <biehlerj@gmail.com>
2020-02-12 13:45:37 +0000

    Formatting changes

--- models/base_model.py
+++ models/base_model.py
@@ -10,18 +10,20 @@ import uuid
 
 
 class BaseModel:
-    """ 
-    Class: Base 
+    """
+    Class: Base
     """
     def __init__(self, *args, **kwargs):
         '''
         Initializes an instance
         '''
         if kwargs is not None:
-            kwargs['created_at'] = datetime.strftime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs['updated_at'] = datetime.strftime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs['created_at'] = datetime.strftime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs['updated_at'] = datetime.strftime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
             for key, value in kwargs.items():
-                setattr(self, key, value)		
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
