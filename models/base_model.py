b12b56abbf52d8e93de040c86568b14bc244f98b
Jacob Biehler <biehlerj@gmail.com>
2020-02-12 10:56:17 +0000

    Updating branch

---
+++ models/base_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+File for class BaseModel and all its methods and data
+'''
+
+
+import uuid
+import datetime
+import cmd
+
+
+class BaseModel(self):
+    '''
+    Defining all common attributes/methods for other classes
+    '''
+    def __init__(self):
+        '''
+        Initialization for an instance of BaseModel
+        '''
