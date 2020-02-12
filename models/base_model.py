5021cf4d504ba1809b1f3636afa336e81188aee9
Jerel <henderson.jerel@yahoo.com>
2020-02-12 12:04:13 +0000

    Update Base Model, and add Task 3 test

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
         """ Initialization method """
         if hasattr(self, "created_at") and type(self.created_at) is str:
             self.created_at = datetime
-        if hassattr(self, "updated_at") and type(self.updated_at) is str:
+        if hasattr(self, "updated_at") and type(self.updated_at) is str:
             self.updated_at = datetime
         else:
             self.id = str(uuid.uud4())
